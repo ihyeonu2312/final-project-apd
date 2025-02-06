c987eb90680c2d9f782779b5411aa35b6fc13beb
sangwon1233 <tkddnjs3351@naver.com>
2025-02-06 15:36:13 +0900

    test 코드 및 의존성 추가

--- apd/src/test/java/site/unoeyhi/apd/repository/MemberRepositoryTest.java
+++ apd/src/test/java/site/unoeyhi/apd/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package site.unoeyhi.apd.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+import jakarta.transaction.Transactional;
 import site.unoeyhi.apd.entity.Member;
 
 import java.time.LocalDateTime;
@@ -13,12 +17,17 @@ public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
     @Test
+    @Transactional
+    @Rollback(false)
     public void testMemberSave() {
         // 데이터 생성
         Member member = Member.builder()
                 .email("test@example.com")
-                .password("password123")
+                .password(encoder.encode("1234"))
                 .nickname("testNickname")
                 .phoneNumber("010-1234-5678")
                 .address("서울시 강남구")
@@ -27,8 +36,10 @@ public class MemberRepositoryTest {
                 .updatedAt(LocalDateTime.now())
                 .build();
 
+        
         // 데이터 저장
         memberRepository.save(member);
+        memberRepository.flush();  // 즉시 DB에 반영
 
         // 저장 확인 (optional)
         System.out.println("Saved member ID: " + member.getMemberId());

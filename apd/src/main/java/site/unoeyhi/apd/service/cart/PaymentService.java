a30d81e1e0f600b8c1e005f3b245c1e3989f1297
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-04-24 17:06:48 +0900

    httpclient 버전 수정

--- apd/src/main/java/site/unoeyhi/apd/service/cart/PaymentService.java
+++ apd/src/main/java/site/unoeyhi/apd/service/cart/PaymentService.java
@@ -2,7 +2,6 @@ package site.unoeyhi.apd.service.cart;
 
 import site.unoeyhi.apd.dto.cart.PaymentInitiateResponseDto;
 import site.unoeyhi.apd.dto.cart.PaymentRequestDto;
-import site.unoeyhi.apd.dto.cart.PaymentResponseDto;
 
 public interface PaymentService {
     PaymentInitiateResponseDto initiatePayment(Long orderId, PaymentRequestDto requestDto);

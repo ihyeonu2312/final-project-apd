d93b7a1e99569e18df0218fd195cb723f36142f6
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-04-24 18:29:15 +0900

    devtoolcontroller 수정

--- apd/src/main/java/site/unoeyhi/apd/controller/DevToolController.java
+++ apd/src/main/java/site/unoeyhi/apd/controller/DevToolController.java
@@ -1,19 +1,19 @@
-package site.unoeyhi.apd.controller;
+// package site.unoeyhi.apd.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import site.unoeyhi.apd.service.CloudinaryUploadService;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.web.bind.annotation.*;
+// import site.unoeyhi.apd.service.CloudinaryUploadService;
 
-@RestController
-@RequestMapping("/api/dev")
-@RequiredArgsConstructor
-public class DevToolController {
+// @RestController
+// @RequestMapping("/api/dev")
+// @RequiredArgsConstructor
+// public class DevToolController {
 
-    private final CloudinaryUploadService cloudinaryUploadService;
+//     private final CloudinaryUploadService cloudinaryUploadService;
 
-    @PostMapping("/upload-cloudinary")
-    public String triggerCloudinaryUpload() {
-        cloudinaryUploadService.uploadAndUpdateImages();
-        return "✅ Cloudinary 업로드 및 DB 업데이트 완료!";
-    }
-}
+//     @PostMapping("/upload-cloudinary")
+//     public String triggerCloudinaryUpload() {
+//         cloudinaryUploadService.uploadAndUpdateImages();
+//         return "✅ Cloudinary 업로드 및 DB 업데이트 완료!";
+//     }
+// }

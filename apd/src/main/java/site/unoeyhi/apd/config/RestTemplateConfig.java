878a52505059c7ac78ba14eebdf859710378aca7
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-04-24 16:56:52 +0900

    httpclient 버전 수정

--- apd/src/main/java/site/unoeyhi/apd/config/RestTemplateConfig.java
+++ apd/src/main/java/site/unoeyhi/apd/config/RestTemplateConfig.java
@@ -1,15 +1,36 @@
 package site.unoeyhi.apd.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
+
 @Configuration
 public class RestTemplateConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate(); 
-    }
 
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        // Trust all certificates (Optional: 사용 시 위험 요소 있음, 테스트 목적)
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return new RestTemplate(factory);
+    }
 }

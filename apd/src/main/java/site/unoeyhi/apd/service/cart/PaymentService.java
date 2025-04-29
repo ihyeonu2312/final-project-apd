869e91a6e96aa60dead2d3ec56aee931fca9d79c
ihyeonu <dlgusdn2312@gmail.com>
2025-04-29 18:20:42 +0900

    250429

--- apd/src/main/java/site/unoeyhi/apd/service/cart/PaymentService.java
+++ apd/src/main/java/site/unoeyhi/apd/service/cart/PaymentService.java
@@ -1,8 +1,8 @@
 package site.unoeyhi.apd.service.cart;
 
-import site.unoeyhi.apd.dto.cart.PaymentInitiateResponseDto;
+import site.unoeyhi.apd.dto.cart.PaymentResponseDto;
 import site.unoeyhi.apd.dto.cart.PaymentRequestDto;
 
 public interface PaymentService {
-    PaymentInitiateResponseDto initiatePayment(Long orderId, PaymentRequestDto requestDto);
+    PaymentResponseDto initiatePayment(Long orderId, PaymentRequestDto requestDto);
 }

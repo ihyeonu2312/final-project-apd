e556ede19b69879d1be92b84cd744d4bc045c060
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-07-28 15:25:31 +0900

    json 형식 데이터 받기

--- apd/src/main/java/site/unoeyhi/apd/dto/cart/PaymentInitiateResponseDto.java
+++ apd/src/main/java/site/unoeyhi/apd/dto/cart/PaymentInitiateResponseDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class PaymentInitiateResponseDto {
-    private String redirectUrl;                // ex) https://mobile.inicis.com/smart/payment/
+    private String redirectUrl;                
     private InicisPaymentRequestDto requestData;  // form 으로 넘길 데이터
 }
  
